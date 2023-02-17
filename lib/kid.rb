f199468c807b33b798f58a8576085871cd6e0654
tony555t <tonytullah764@gmail.com>
2023-02-17 12:06:08 +0300

    module

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,12 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance_module'
+class Kid
+include FancyDance::InstanceMethod
+extend FancyDance::InstanceMethod
+attr_accessor :name
+
+def initialize(name)
+    @name = name
+end
+end
